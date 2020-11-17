8765e5c5460b5824ffc5dc965c8d9c324e4c0980
Danial Kafeel <danialkafeel@gmail.com>
2020-11-17 19:24:58 +0530

    updated

--- ssd33/settings.py
+++ ssd33/settings.py
@@ -11,12 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
-# print("sdfsdf = ",SETTINGS_PATH)
-print("BASE_DIR = ",BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
